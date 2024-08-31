91fa3fa202e172889d31842506411480c353868d
s7fy <141638237+s7fy@users.noreply.github.com>
2024-08-31 18:36:22 +0900

    Update RawSocketTest.py
    add jumbo frame mode

--- RawSocketTest.py
+++ RawSocketTest.py
@@ -4,12 +4,16 @@ import time
 import sys
 
 #イーサネットフレーム送信関数
-def send_frame(interface, src_mac_addr, payload):
-    ether_frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac_addr, type=0x0800) / payload #宛先のMACアドレスはブロードキャスト、送信元は指定したMACアドレス、タイプフィールドはIPv4
+def send_frame(interface, src_mac_addr, payload, is_jumbo):
+    if is_jumbo == True:
+        payload = "A"*1504 #Windows 10では、FCSフィールドを検出できないため、データフィールドをかさ増し(1504byte)
+    else:
+        pass
+    ether_frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac_addr, type=0x88B5) / Raw(load=payload) #宛先のMACアドレスはブロードキャスト、送信元は指定したMACアドレス、タイプフィールドはIPv4
     sendp(ether_frame, iface=interface, verbose=0)
 
 #イーサネットフレーム受信関数
-def receive_frame(interface, src_mac_addr, timeout=5):
+def receive_frame(interface, src_mac_addr, timeout=2):
     def packet_handler(packet):
         if packet.haslayer(Ether): #イーサネットレイヤーが存在するか
             eth = packet.getlayer(Ether)
@@ -20,16 +24,29 @@ def receive_frame(interface, src_mac_addr, timeout=5):
 
     sniff(iface=interface, prn=packet_handler, timeout=timeout)
 
+def is_jumbo(jumbo_mode):
+    if jumbo_mode.lower() == "on":
+        return True
+    elif jumbo_mode.lower() == "off":
+        return False
+    else:
+        print(f"Usage  : python3 {sys.argv[0]} [interface_name] [src_mac_address] [payload] [jumbo frame mode: on/off]")
+        print(f"Example: python3 {sys.argv[0]} \"イーサネット 2\" 00:00:00:00:00:00 \"Message\" off" )
+        exit(1)
+
 #メイン関数
 def main():
-    if len(sys.argv) != 4:
-        print(f"Usage  : python3 {sys.argv[0]} [interface_name] [src_mac_address] [payload]")
-        print(f"Example: python3 {sys.argv[0]} \"イーサネット 2\" 00:00:00:00:00:00 \"Message\"" )
+    if len(sys.argv) != 5:
+        print(f"Usage  : python3 {sys.argv[0]} [interface_name] [src_mac_address] [payload] [jumbo frame mode: on/off]")
+        print(f"Example: python3 {sys.argv[0]} \"イーサネット 2\" 00:00:00:00:00:00 \"Message\" off" )
         exit(1)
 
     interface = sys.argv[1] #インターフェイス名を指定
     src_mac_addr = sys.argv[2] #自分のMACアドレス
     payload = sys.argv[3] #ペイロードを指定
+    jumbo_mode = sys.argv[4] #ジャンボフレームにするかどうか
+
+    is_jumbo_mode = is_jumbo(jumbo_mode)
 
     # 受信スレッドを立ち上げ
     receive_thread = threading.Thread(target=receive_frame, args=(interface, src_mac_addr))
@@ -37,7 +54,7 @@ def main():
     time.sleep(1) # 受信側が準備できるように少し待つ
     
     # 送信スレッドを立ち上げ
-    send_thread = threading.Thread(target=send_frame, args=(interface, src_mac_addr, payload))
+    send_thread = threading.Thread(target=send_frame, args=(interface, src_mac_addr, payload, is_jumbo_mode))
     send_thread.start()
 
     send_thread.join()
